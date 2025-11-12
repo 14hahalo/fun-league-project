491752e3c73f6650728e9fba22b0e07632b89c39
Halil Can TOPTAŞ <halilcantoptas@gmail.com>
2025-11-12 16:24:35 +0300

    env edit

--- domain/src/app.ts
+++ domain/src/app.ts
@@ -8,19 +8,19 @@ const app: Application = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://192.168.0.82:5173"],
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
   })
 );
 
 app.use(
   compression({
-    level: 6, 
-    threshold: 1024, 
+    level: 6,
+    threshold: 1024,
   })
 );
 
-app.use(express.json({ limit: "10mb" })); 
+app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.use("/api", routes);
