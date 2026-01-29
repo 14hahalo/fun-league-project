c95282036ad9fa61a671310854e0c796fa013478
Halil Can TOPTAŞ <halilcantoptas@gmail.com>
2026-01-29 13:31:43 +0300

    febUp3

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,8 @@
 import app from '../domain/dist/app';
 
-export default app;
+const handler = (req: any, res: any, next: any) => {
+  req.url = `/api${req.url}`;
+  return app(req, res, next);
+};
+
+export default handler;
