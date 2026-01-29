801dae0d37e16334379a66cdcc80af3880603865
Halil Can TOPTAŞ <halilcantoptas@gmail.com>
2026-01-29 14:30:40 +0300

    vercel env

--- domain/src/config/firebase.ts
+++ domain/src/config/firebase.ts
@@ -1,18 +1,25 @@
 import admin from 'firebase-admin';
 import * as path from 'path';
 
-let serviceAccount: admin.ServiceAccount;
+if (!admin.apps.length) {
+  let serviceAccount: admin.ServiceAccount;
 
-if (process.env.FIREBASE_SERVICE_ACCOUNT) {
-  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-} else {
-  const serviceAccountPath = path.join(__dirname, '../../../serviceAccountKey.json');
-  serviceAccount = require(serviceAccountPath);
+  if (process.env.FIREBASE_SERVICE_ACCOUNT_BASE64) {
+    // Base64 encoded JSON (recommended for Vercel)
+    const decoded = Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, 'base64').toString('utf-8');
+    serviceAccount = JSON.parse(decoded);
+  } else if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    // Plain JSON (may have issues with newlines in private_key)
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  } else {
+    const serviceAccountPath = path.join(__dirname, '../../../serviceAccountKey.json');
+    serviceAccount = require(serviceAccountPath);
+  }
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
 }
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
 
 const db = admin.firestore();
 
